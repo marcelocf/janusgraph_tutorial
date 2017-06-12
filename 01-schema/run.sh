c63b12384168207048094db891c115f695ebab31
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-06-12 15:22:22 +0900

    add base project

---
+++ 01-schema/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this will compile, extract the product zip file and then run the comand
+
+
+echo Building...
+./gradlew distZip
+
+
+echo Extracting
+pushd build/distributions
+unzip -q -o schema.zip
+
+
+echo Running
+pushd schema
+./bin/schema
+
+popd && popd
---
+++ 01-schema/src/main/java/marcelocf/janusgraph/Schema.java
@@ -0,0 +1,9 @@
+
+package marcelocf.janusgraph;
+
+public class Schema {
+
+  public static void main(String[] argv) {
+    System.out.println("OMG");
+  }
+}
