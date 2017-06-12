60f990a3f5f55faec1d22f59c518bf4a112d4ed3
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-06-12 17:57:48 +0900

    make sure it builds fine with simpler build file

--- 01-schema/run.sh
+++ 01-schema/run.sh
@@ -3,17 +3,12 @@
 # this will compile, extract the product zip file and then run the comand
 
 
-echo Building...
-./gradlew distZip
-
-
-echo Extracting
-pushd build/distributions
-unzip -q -o schema.zip
-
-
-echo Running
-pushd schema
-./bin/schema
-
-popd && popd
+echo Building... && \
+  ./gradlew distZip
+  echo Extracting  && \
+  pushd build/distributions && \
+  unzip -q -o schema.zip && \
+  echo Running && \
+  pushd schema && \
+  ./bin/schema && \
+  popd && popd
