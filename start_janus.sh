6a25096cf9e784e94bcbce3636d9026c30da6dfe
Marcelo C. Freitas <marcelo.freitas@finc.com>
2017-06-13 12:09:09 +0900

    add script to run janus graph

---
+++ start_janus.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# this will make sure janus is downloaded and running
+
+
+JANUS_VERSION=0.1.0
+WORKDIR="$(dirname $0)/work"
+
+JDIR=janusgraph-${JANUS_VERSION}-hadoop2
+PKGNAME=${JDIR}.zip
+
+JPATH="${WORKDIR}/${JDIR}"
+
+function janus_exists(){
+  test -d "${JPATH}"
+}
+
+function download_janus(){
+  echo Downloading janus
+  mkdir -p $WORKDIR
+  cd "$WORKDIR"
+  wget  https://github.com/JanusGraph/janusgraph/releases/download/v${JANUS_VERSION}/${PKGNAME}
+  echo Extracting
+  unzip -o -q $PKGNAME
+}
+
+
+janus_exist || download_janus
+
+echo starting janus
+cd "$JPATH" && ./bin/janusgraph.sh start
