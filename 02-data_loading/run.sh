5e4ac9e64ab603e81d16c5f0e69c7b53722864b5
Marcelo C. Freitas <marcelo.freitas@finc.com>
2017-06-13 18:23:35 +0900

    add run script

---
+++ 02-data_loading/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this will compile, extract the product zip file and then run the comand
+
+
+echo Building... && \
+  ./gradlew distZip
+  echo Extracting  && \
+  pushd build/distributions && \
+  unzip -q -o data_loading && \
+  echo Running && \
+  pushd data_loading && \
+  ./bin/data_loading && \
+  popd && popd
