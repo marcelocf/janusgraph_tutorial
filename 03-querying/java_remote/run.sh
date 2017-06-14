7515ce31f15ca26f98df418474f877438d57bff5
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-06-14 14:47:57 +0900

    working example for remote quries

---
+++ 03-querying/java_remote/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this will compile, extract the product zip file and then run the comand
+
+
+echo Building... && \
+  ./gradlew distZip
+  echo Extracting  && \
+  pushd build/distributions && \
+  unzip -q -o java_remote && \
+  echo Running && \
+  pushd java_remote && \
+  ./bin/java_remote && \
+  popd && popd
---
+++ 03-querying/java_remote/src/main/java/marcelocf/janusgraph/RemoteQueries.java
@@ -0,0 +1,90 @@
+package marcelocf.janusgraph;
+
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import static marcelocf.janusgraph.Schema.USER_NAME;
+import static org.apache.tinkerpop.gremlin.process.traversal.P.eq;
+
+public class RemoteQueries {
+
+  public static final String CONFIG_FILE = "../../../../../conf/janusgraph-remote.properties";
+  private static final Logger LOGGER = LoggerFactory.getLogger(RemoteQueries.class);
+  private final Graph graph;
+  private final GraphTraversalSource g;
+
+  /**
+   * Run every example query, outputting results via @LOGGER
+   *
+   * @param argv
+   * @throws Exception
+   */
+  public static void main(String[] argv) throws Exception {
+    RemoteQueries remoteQueries = new RemoteQueries();
+    String userName = "testUser0";
+
+    System.out.println("Initialized the remote query executor");
+
+
+    System.out.println("Getting user:");
+    print(remoteQueries.getUser(userName));
+
+    System.out.println("Getting followed users");
+    print(remoteQueries.getFollowedUsers(userName));
+
+    System.out.println("Getting followers users");
+    print(remoteQueries.getFollowers(userName));
+
+    remoteQueries.close();
+    System.exit(0);
+  }
+
+  /**
+   * Just a simple print method for every property returned from a vertex traversal
+   * @param traversal
+   */
+  private static void print(GraphTraversal<Vertex, Vertex> traversal) {
+    GraphTraversal<Vertex, Map<String, Object>> valueMap = traversal.valueMap(true);
+    int count = 0;
+
+    for (GraphTraversal<Vertex, Map<String, Object>> it = valueMap; it.hasNext(); ) {
+      Map<String, Object> item = it.next();
+      LOGGER.info(" {}: {} ", count++, item.toString());
+      System.out.println(item.toString());
+    }
+    LOGGER.info("Printed {} element(s)", count);
+  }
+
+
+  // Instance Methods //
+  ///////
+
+  public RemoteQueries() throws Exception {
+    graph = EmptyGraph.instance();
+    g = graph.traversal().withRemote(CONFIG_FILE);
+  }
+
+  public void close() throws Exception {
+    g.close();
+    // no need to close the graph because it is an empty graph allocated only to be able to connect to a remote
+  }
+
+  public GraphTraversal<Vertex, Vertex> getUser(String userName) {
+    return g.V().hasLabel(Schema.USER).has(USER_NAME, eq(userName));
+  }
+
+  public GraphTraversal<Vertex, Vertex> getFollowedUsers(String userName) {
+    return getUser(userName).out(Schema.FOLLOWS);
+  }
+
+  public GraphTraversal<Vertex, Vertex> getFollowers(String userName) {
+    return getUser(userName).in(Schema.FOLLOWS);
+  }
+}
