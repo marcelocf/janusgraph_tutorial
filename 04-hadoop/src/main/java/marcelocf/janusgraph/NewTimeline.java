2cdb645589c2c09ced35dad298b46fc7dd5a40b6
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-07-13 18:01:26 +0900

    rename our timeline

--- 04-hadoop/src/main/java/marcelocf/janusgraph/NewTimeline.java
+++ 04-hadoop/src/main/java/marcelocf/janusgraph/NewTimeline.java
@@ -5,24 +5,22 @@ import org.janusgraph.core.JanusGraphFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
-
 /**
  * Prints out the timeline to stdout
  * <br/>
  * Created by marcelo on 17/06/21.
  */
-public class Timeline {
+public class NewTimeline {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Timeline.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(NewTimeline.class);
   public static void main(String argv[]) throws Exception {
     JanusGraph graph = JanusGraphFactory.open(Schema.CONFIG_FILE);
     HadoopQueryRunner q = new HadoopQueryRunner(graph.traversal(), "testUser1");
     int runs = 10;
 
     for(int i =0; i < runs; i++) {
-      LOGGER.info("Previous Timeline (run {} of {})", i+1, runs);
-      LOGGER.info("New Timeline");
+      LOGGER.info("Previous NewTimeline (run {} of {})", i+1, runs);
+      LOGGER.info("New NewTimeline");
       q.printTimeline(q.getTimeline3(10));
     }
     q.close();
