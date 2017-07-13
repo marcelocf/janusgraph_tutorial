2d6ec792022c0090fe5f7d0cb783b0c6fa3302e8
フレイタス　マルセロ <marcelo.freitas@finc.com>
2017-07-13 18:00:57 +0900

    separate old from new timeline

--- 04-hadoop/src/main/java/marcelocf/janusgraph/OldTimeline.java
+++ 04-hadoop/src/main/java/marcelocf/janusgraph/OldTimeline.java
@@ -5,16 +5,14 @@ import org.janusgraph.core.JanusGraphFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
-
 /**
  * Prints out the timeline to stdout
  * <br/>
  * Created by marcelo on 17/06/21.
  */
-public class Timeline {
+public class OldTimeline {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Timeline.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(OldTimeline.class);
   public static void main(String argv[]) throws Exception {
     JanusGraph graph = JanusGraphFactory.open(Schema.CONFIG_FILE);
     HadoopQueryRunner q = new HadoopQueryRunner(graph.traversal(), "testUser1");
@@ -22,8 +20,7 @@ public class Timeline {
 
     for(int i =0; i < runs; i++) {
       LOGGER.info("Previous Timeline (run {} of {})", i+1, runs);
-      LOGGER.info("New Timeline");
-      q.printTimeline(q.getTimeline3(10));
+      q.printTimeline(q.getTimeline2(10));
     }
     q.close();
     graph.close();
